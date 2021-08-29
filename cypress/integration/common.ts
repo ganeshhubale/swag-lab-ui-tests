94da715dd8663212d9a79b55db7a60c4c4305384
Ganesh Hubale <ganeshhubale03@gmail.com>
2021-08-29 16:20:02 +0530

    Automate login test test cases
    Signed-off-by: Ganesh Hubale <ganeshhubale03@gmail.com>

---
+++ cypress/integration/common.ts
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+import * as loginview from "../integration/views/login.view";
+import * as productview from "../integration/views/products.view";
+import { hopePageTitle } from "../integration/constants";
+
+const hostUrl = Cypress.env("hostUrl");
+const username = Cypress.env("username");
+const password = Cypress.env("password");
+
+export function login(): void {
+  cy.visit(hostUrl);
+  cy.get(loginview.usernameInput).clear().type(username);
+  cy.get(loginview.passwordInput).clear().type(password);
+  cy.get(loginview.logInButton).click();
+
+  cy.wait(1000);
+  cy.get(productview.productTitle).should("contain", hopePageTitle);
+}
---
+++ cypress/integration/views/login.view.ts
@@ -0,0 +1,6 @@
+export const usernameInput = "input[placeholder='Username']";
+export const passwordInput = "input[id=password]";
+export const logInButton = "input[id=login-button]";
+
+export const loginErrorAlert =
+  "div[class='error-message-container error'] > h3";
