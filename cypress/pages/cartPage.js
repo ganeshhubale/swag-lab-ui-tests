74e284344ce2fbda075c555b0cf01324f5a4e181
Ganesh Hubale <ganeshhubale03@gmail.com>
2025-01-08 18:52:12 +0530

    Automated checkout functionality scenarios
    Signed-off-by: Ganesh Hubale <ganeshhubale03@gmail.com>

--- cypress/pages/cartPage.js
+++ cypress/pages/cartPage.js
@@ -1,7 +1,8 @@
 class Cart{
 
     elements = {
-        cartBtn: () => cy.get("div[id=shopping_cart_container]")
+        cartBtn: () => cy.get("div[id=shopping_cart_container]"),
+        checkoutbtn: () => cy.get("button[id=checkout]")
     }
 
     removeFromCart(itemNames){
@@ -10,5 +11,9 @@ class Cart{
             cy.get(`button[id='remove-${name}']`).click();
         });
     };
+
+    checkoutCart(){
+        this.elements.checkoutbtn().click();
+    }
 };
 module.exports = new Cart();
